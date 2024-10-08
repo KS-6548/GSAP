b0ebf549a9876abc58466d9b1891fc5896912b60
KS-6548 <karthiksundram871@gmail.com>
2024-10-08 14:23:21 +0530

    gsap file

---
+++ app.js
@@ -0,0 +1,12 @@
+gsap.registerPlugin(TextPlugin)
+
+const myvar = document.getElementById("var")
+
+gsap.to(myvar,{
+    duration : 2,
+    text:{
+        value:"The Code Creative",
+        delimiter:" "
+    }
+    
+})
